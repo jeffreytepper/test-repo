a2af55bbd563126401e07f22444c76976bd24599
jeffreytepper <teppermax@gmail.com>
2022-10-20 17:27:54 -0500

    next commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/index.html');
-    res.sendStatus(200);
+    res.sendFile(__dirname + '/index.html');
+    //res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
