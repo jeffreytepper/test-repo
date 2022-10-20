6a3bc81cf5b38351a90b0bd11269801ca98c647e
jeffreytepper <teppermax@gmail.com>
2022-10-20 17:05:35 -0500

    add frontend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendStatus(200);
+    res.sendFile(__dirname + '/index.html');
 });
 
 const PORT = process.env.PORT || 3000;
