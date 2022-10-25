091b51753eabcbd760afde187c8737b92f5d73df
jeffreytepper <teppermax@gmail.com>
2022-10-24 22:33:57 -0500

    ready for deployment

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.get('/', (req, res) => {
     //res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5001;
+const start = async() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
+
+start();
